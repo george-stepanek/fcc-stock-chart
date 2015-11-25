477259fc68b2cb15930779acdc616b7e7e8f037e
stepang <g.stepanek.nz@gmail.com>
2015-11-25 02:58:01 +0000

    Implement stock data retrievl and display

--- app/controllers/controller.client.js
+++ app/controllers/controller.client.js
@@ -2,97 +2,81 @@
 
 (function () {
    
+   google.load("visualization", "1", {packages:["corechart"], "callback" : displayStocks});
+   
+   var colors = ['blue', 'red', 'green', 'purple'];
+   var stocks = ['AAPL', 'MSFT', 'FB'];
+   
+   $('input').focus();
    $('input').keypress(function(e){
       if(e.keyCode == 13)
-         $('.find-bars').click();
-   });
-   $('input').focus();
-         
-   var userid = null;
-   var goingTo = '';
-   var offset = 0;
-   
-   function apiUrl () {
-      return window.location.origin + '/api/bars/' + encodeURIComponent($('input').val()) + "?offset=" + offset;
-   }
-
-   function displayBars (results) {
-      var output = "";
-      for(var i = 0; i < results.businesses.length; i++) {
-         output = output + '<div class="row">' +
-            '<div class="col-sm-1">' + (results.businesses[i].image_url != undefined ? 
-               '<img src="' + results.businesses[i].image_url + '"></img>' : '') + '</div>' + 
-            '<div class="col-sm-2"><a target="_blank" href="' + results.businesses[i].url + 
-               '"><h4>' + results.businesses[i].name + '</h4></a></div>' + 
-            '<div class="col-sm-7"><i>' + (results.businesses[i].snippet_text != undefined ? 
-               '"' + results.businesses[i].snippet_text + '"': '') + '</i></div>' +
-            '<div class="col-sm-1"><button class="btn is-going' + (results.businesses[i].id == goingTo ? ' btn-primary' : '') +
-               '" ' + (userid == null ? "disabled" : "") + ' id="' + results.businesses[i].id + '">' + 
-                  results.businesses[i].going + ' going</button></div>' +
-            '<div class="col-sm-1" style="width:70px; text-align:right;"><span>(' + (offset + i + 1) + ')</span></div></div>';
-      }
-      output = output + '<div class="row">' +
-         '<div class="col-sm-1"><button class="btn find-prev"' + (offset <= 0 ? ' disabled' : '') + '>' +
-            '<span class="glyphicon glyphicon-triangle-left"></span> Prev</button></div>' +
-         '<div class="col-sm-9"></div>' +
-         '<div class="col-sm-1"><button class="btn find-next"' + (offset >= results.total - 10 ? 'disabled' : '') +
-            '>Next <span class="glyphicon glyphicon-triangle-right"></span></button></div></div>';
-         
-      $('#results').html(output);
-      
-      $('.find-prev').click( function () {
-         offset -= 10;
-         findBars();
-      });
-      
-      $('.find-next').click( function () {
-         offset += 10;
-         findBars();
-      });
-      
-      $('.is-going').click( function () {
-         if($(this).hasClass('btn-primary')) {
-            goingTo = '';
-            $.post(apiUrl() + "&goingto=", function (results) { findBars(); });       
-         } else {
-            goingTo = this.id;
-            $.post(apiUrl() + "&goingto=" + this.id, function (results) { findBars(); });
-         }
-      });
-   }
-
-   function findBars () {
-      $.cookie("city", $('input').val());
-      $.cookie("offset", offset);
-      $.get(apiUrl(), displayBars);
-   }
-   
-   $('.find-bars').click( function () {
-      offset = 0;
-      findBars();
+         $('.find-stock').click();
    });
 
-   $("document").ready($.get(window.location.origin + '/api/:id', function (user) {
-      $('#profile-photo').html("<img src='" + user.photo + "'></img>");
-      $('#login').hide();
-      $('#logout').show();
-      userid = user.id;
-      goingTo = user.goingTo;
+   $('.find-stock').click(findStock);
+   $(window).on('resize', displayStocks);
       
-      // recommended fix for facebook authentication bug
-      if (window.location.hash && window.location.hash === "#_=_") {
-         if (window.history && window.history.pushState) {
-            window.history.pushState("", document.title, window.location.pathname);
-         } else {
-            location.hash = "";
-         }
+   function findStock () {
+      
+      stocks.push($('input').val());
+      $('input').val('');
+      $('input').blur();
+      displayStocks();
+   }
+   
+   function displayStocks () {
+
+      var vals = [];
+      //$('#stock-buttons').html("");
+      
+      for(var stock = 0; stock < stocks.length; stock++ ) {
+         $('#stock-buttons').append('<button class="btn stock-button" style="color: ' + colors[stock] + '">' + stocks[stock] + 
+            '&nbsp;&nbsp;&nbsp;<span class="glyphicon glyphicon-remove-sign"></span></button>');
+         
+         var apiUrl = "https://www.quandl.com/api/v3/datasets/WIKI/" + stocks[stock] +
+            ".json?start_date=2015-01-01&end_date=2016-01-01&order=asc&column_index=4&api_key=yxUrG7eH5x2MHTCQG_1M";
+
+         $.ajax({url: apiUrl, async: false, success: function(results) {
+            if(vals.length == 0) {
+               for(var i = 0; i < results.dataset.data.length; i++) {
+                  vals.push([new Date(results.dataset.data[i][0]), results.dataset.data[i][1]]);
+               }
+            } else {
+               for(var i = 0; i < results.dataset.data.length; i++) {
+                  var val = vals.filter(function (value) {
+                     var date = new Date(results.dataset.data[i][0]);
+                     return(value[0].getTime() == date.getTime());
+                  });
+                  if(val.length > 0) {
+                     val[0].push(results.dataset.data[i][1]);
+                  }
+               }
+               
+               // fill in any gaps
+               for(var i = 0; i < vals.length; i++) {
+                  if(vals[i].length < stock + 2) {
+                     vals[i].push(null);
+                  }
+               }
+            }
+         }});
       }
       
-      if($.cookie("city")) {
-         $('input').val($.cookie("city"));
-         offset = parseInt($.cookie("offset"));
-         findBars();
-         $('input').blur();
+      var data = new google.visualization.DataTable();
+      data.addColumn('date', 'Date');
+      for(var i = 0; i < stocks.length; i++ ) {
+         data.addColumn('number', stocks[i]);
       }
-   }));
+      data.addRows(vals);
+
+      var chart = new google.visualization.LineChart(document.getElementById('chart_div'));
+      chart.draw(data, { 
+         height: $(window).height() / 2,
+         width: $(window).width(),
+         legend: { position: 'none' }, 
+         crosshair: { trigger: 'focus' }, 
+         colors: colors, 
+         interpolateNulls: true });
+   }
+
 })();
--- app/routes/index.js
+++ app/routes/index.js
@@ -1,43 +1,8 @@
 'use strict';
 
 var path = process.cwd();
-var Handler = require(path + '/app/controllers/handler.server.js');
 
-module.exports = function (app, passport) {
+module.exports = function (app) {
 
-	function isLoggedIn (req, res, next) {
-		if (req.isAuthenticated()) {
-			return next();
-		}
-	}
-
-	app.route('/')
-		.get(function (req, res) {
-			res.sendFile(path + '/public/index.html');
-		});
-
-	app.route('/logout')
-		.get(function (req, res) {
-			req.logout();
-			res.redirect('/');
-		});
-
-	app.route('/api/:id')
-		.get(isLoggedIn, function (req, res) {
-			res.json(req.user);
-		});
-
-	app.route('/auth/facebook')
-		.get(passport.authenticate('facebook'));
-
-	app.route('/auth/facebook/callback')
-		.get(passport.authenticate('facebook', {
-			successRedirect: '/',
-			failureRedirect: '/'
-		}));
-		
-	var handler = new Handler();
-	app.route('/api/bars/:city')
-		.get(handler.getBars)
-		.post(handler.addGoing);
+	app.route('/').get(function (req, res) { res.sendFile(path + '/public/index.html');	});
 };
